b6db28b249f66db0f180184993e2a0a059ee5250
Indumathi Balasubramaniyan <indu21nov@gmail.com>
2026-07-11 15:29:26 +0530

    Add basic Streamlit app

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="Walmart M5 Demand Forecasting",
+    page_icon="🛒",
+    layout="wide"
+)
+
+st.title("🛒 Walmart M5 Demand Forecasting")
+
+st.write("Retail Demand Forecasting using LightGBM Machine Learning")
